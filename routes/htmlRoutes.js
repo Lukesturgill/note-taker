40bbe4e696556ec0fcf2a65dc39b58280e6e8853
Luke Sturgill <lukesturgill@gmail.com>
2022-06-19 16:53:48 -0400

    fixed routing errors in html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,12 @@ const router = require('express').Router();
 
 //send notes to note.html tempelet
 router.get('/notes', (req, res)=> {
-    res.sendFile(path.join('../public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
 
 //send to homepage if issue occures
 router.get('/', (req, res) => {
-    red.sendFile(path.join('../public/index.html'))
+    red.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
 module.exports = router;
