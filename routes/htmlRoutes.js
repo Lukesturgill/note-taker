44c2ec3ac120e62c6d187078e0850408abde7bf1
Luke Sturgill <lukesturgill@gmail.com>
2022-06-19 16:42:41 -0400

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,15 @@
 const path = require('path');
-const route = require('express').Router();
+
+const router = require('express').Router();
+
+//send notes to note.html tempelet
+router.get('/notes', (req, res)=> {
+    res.sendFile(path.join('../public/notes.html'))
+});
+
+//send to homepage if issue occures
+router.get('/', (req, res) => {
+    red.sendFile(path.join('../public/index.html'))
+});
+
+module.exports = router;
